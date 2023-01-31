6a4d45ea5d80559f4f5140939ee7824debad15b1
luizaamaria <luizamariabarros.contato@gmail.com>
2023-01-31 15:21:47 -0300

    Incluido função e tela de autenticação

--- core/views.py
+++ core/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # def index(request):
 #     return redirect('/agenda/')
+
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuario ou senha inválido")
+    return redirect('/')
+@login_required(login_url='/login/') #se não estiver autenticado não irá abrir a página da agenda e quando não estiver autenticado será levado até a página de autenticar.
 def lista_eventos(request):
     usuario = request.user
-    evento = Evento.objects.all()
+    evento = Evento.objects.filter(usuario=usuario)
     dados ={'eventos': evento}
     return render(request, 'agenda.html', dados)
