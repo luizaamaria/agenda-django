1bf73f9a1ff226cf881cb5465c1fbfb684defd3e
luizaamaria <luizamariabarros.contato@gmail.com>
2023-02-01 13:38:26 -0300

    Adiocionado funções de alteração e exclusão de dados

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from core.models import Evento
 
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_display = ('id', 'titulo', 'data_evento', 'data_criacao')
     list_filter = ('usuario', 'data_evento',)
 admin .site.register(Evento, EventoAdmin)
 
